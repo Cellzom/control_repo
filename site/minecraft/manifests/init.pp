e6019d0a14941d8358394a98d131021f7ee45365
Cellzom <157081917+Cellzom@users.noreply.github.com>
2024-01-19 08:26:31 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,24 +1,24 @@
 class minecraft {
-  file {'/opt/minecraft':
+  file {‘/opt/minecraft’:
     ensure => directory,
-    }
-  file {/opt/minecraft/server.jar':
+  }
+  file {‘/opt/minecraft/minecraft_server.1.12.2.jar’:
     ensure => file,
-    source => 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
-    }
-   package {'java':
-     ensure => present,
-   }
-   file {'/opt/minecraft/eula.txt':
-     ensure => file,
-     content => 'eula=true',
-   }
-   file {'/etc/systemd/systemn/minecraft.service':
-     ensure => file,
-     source => 'puppet:///modules/minecraft/minecraft.service',
-   }
-   service {'minecraft':
-     ensure => running,
-     enable => true,
+    source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+  }
+  package {‘java’:
+    ensure => present,
+  }
+  file {‘/opt/minecraft/eula.txt’:
+    ensure => file,
+    content => ‘eula=true’
+  }
+  file {‘/etc/systemd/system/minecraft.service’:
+    ensure => file,
+    source => ‘puppet:///modules/minecraft/minecraft.service’,
+  }
+  service { ‘minecraft’:
+    ensure => running,
+    enable => true,
   }
 }
