bc17b9071e0f8a0044e0aeec3a0a9d1709a61fc6
Cellzom <157081917+Cellzom@users.noreply.github.com>
2024-01-19 07:02:10 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDN80bIdnpSH6Q2bz17wQE6+yZJFEayFN9CxZd7DMaKdc2C/9wJqdHWQq/mDt5Sblk90mEMgJ88R36kWQA7PSZaq+A0y24Wp4b1vzs8pxQ1reHCdeEkhHkeYV5MBVkJrSEMWKaKuh25jqnPQAkGOBYtpdyS6DaC+Ma9+mVHmse87+ptxYJKAkeEpW91He9dgaCXduoVRprXly3lEE/Zl1h1NqyA8NGUrUIPxLzlE4XfG6hSIiViHXPrdOfqUYhWIBUpOmfKoHQtt+B9CRagnCbt+aJGE89/lCZEhzZ51Hcm7vDTpJq+EdNq7YQ3Etbk0iXKCKP4Ek0i5bYAKH1Q7t89',
 	}  
 }
